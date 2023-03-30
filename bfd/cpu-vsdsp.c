c2cbb09db30ac4219b1c0b9b9e9d34656d199c22
Birger Koblitz <git@logicog.de>
2023-03-30 21:31:54 +0200

    Initial Framework for opcodes on VSDSP

---
+++ bfd/cpu-vsdsp.c
@@ -0,0 +1,42 @@
+/* BFD support for the VS_DSP processor.
+   Copyright 2008 Free Software Foundation, Inc.
+   Written by Birger Koblitz
+
+   This file is part of BFD, the Binary File Descriptor library.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+#include "sysdep.h"
+#include "bfd.h"
+#include "libbfd.h"
+
+
+const bfd_arch_info_type bfd_vsdsp_arch =
+{
+  32,				/* 32 bits in a word.  */
+  32,				/* 32 bits in an address.  */
+  8,				/*  8 bits in a byte.  */
+  bfd_arch_vsdsp,		/* enum bfd_architecture arch.  */
+  bfd_mach_vsdsp,
+  "vsdsp",			/* Arch name.  */
+  "vsdsp",			/* Printable name.  */
+  2,				/* Unsigned int section alignment power.  */
+  true,				/* The one and only.  */
+  bfd_default_compatible, 	/* Architecture comparison fn.  */
+  bfd_default_scan ,		/* String to architecture convert fn.  */
+  bfd_arch_default_fill,	/* Default fill.  */
+  NULL,				/* Next in list.  */
+  0,
+};
